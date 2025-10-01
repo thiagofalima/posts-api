0e08169196439b09b50661db3d65c39475359925
thiagofalima <thiagofalima@hotmail.com>
2025-10-01 16:44:01 -0300

    feat(database): Initialize SQLAlchemy ORM structure

--- src/app.py
+++ src/app.py
@@ -1,6 +1,24 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, current_app
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
 
 
 def create_app(test_config=None):
@@ -8,7 +26,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.getenv("SECRET_KEY"),
-        DATABASE="posts.sqlite",
+        SQLALCHEMY_DATABASE_URI="sqlite:///posts.sqlite",
     )
 
     if test_config is None:
@@ -18,7 +36,10 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    from . import db
+    # register cli command
+    app.cli.add_command(init_db_command)
+
+    # initialize extensins
     db.init_app(app)
 
     return app
