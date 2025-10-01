91edc69e2be32ef847a0c9e9f35c738035d1e3cb
thiagofalima <thiagofalima@hotmail.com>
2025-10-01 17:20:59 -0300

    feat(models): Create User and Post models

--- src/app.py
+++ src/app.py
@@ -1,24 +1,46 @@
 import os
+
+from typing_extensions import Annotated
+import click
 from dotenv import load_dotenv
 from flask import Flask, current_app
-
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-import click
+from sqlalchemy import Integer, String, ForeignKey, func, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import datetime
 
 class Base(DeclarativeBase):
-  pass
+    pass
+
 
 db = SQLAlchemy(model_class=Base)
 
-@click.command('init-db')
+
+class User(db.Model):
+    _id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"USer(id={self._id!r}, username={self.username!r}"
+
+class Post:
+    _id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    body: Mapped[str] = mapped_column(String, nullable=False)
+    created: Mapped[datetime] = mapped_column(DateTime, default=datetime.datetime.now())
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"USer(id={self._id!r}, title={self.username!r}, author_id{self.author_id!r}"
+
+
+@click.command("init-db")
 def init_db_command():
     """Clear the existing data and create new tables."""
     global db
     with current_app.app_context():
         db.create_all()
-    click.echo('Initialized the database.')
-
+    click.echo("Initialized the database.")
 
 
 def create_app(test_config=None):
@@ -31,7 +53,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
