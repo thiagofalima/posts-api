c4635eb1ae0d50c50d6a8a860b060c62b0de65c4
thiagofalima <thiagofalima@hotmail.com>
2025-10-03 20:15:36 -0300

    feat(user): Create list_users method for users collection visualization

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -6,7 +6,7 @@ from http import HTTPStatus
 # API RESTFull plural pattern
 pages = Blueprint("user", __name__, url_prefix="/users")
 
-def create_user():
+def _create_user():
     data = request.json
     user = User(
         username=data["username"],
@@ -14,15 +14,27 @@ def create_user():
     db.session.add(user)
     db.session.commit()
 
+def _list_users():
+    query = db.select(User)
+    results =  db.session.execute(query).scalars().all()
+    return [
+        {"id": result.id,
+         "username": result.username
+        }
+        for result in results
+    ]
 
 @pages.route("/", methods=["GET", "POST"])
 def hendle_user():
     
     if request.method == "POST":
-        create_user()
-        return {"message": "User created!"}, HTTPStatus.CREATED
+        try:
+            _create_user()
+            return {"message": "User created!"}, HTTPStatus.CREATED
+        except ValueError as e:
+            return {"error": str(e)}, HTTPStatus.BAD_REQUEST
     else:
-        return {"users": []}, HTTPStatus.OK
+        return {"users": _list_users()}, HTTPStatus.OK
     
 
 
