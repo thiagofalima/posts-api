1495cc38a4c11baa262aac90a58982b5b3898d0b
thiagofalima <thiagofalima@hotmail.com>
2025-10-08 16:23:32 -0300

    chore(db): Configure Flask-Migrate and Alembic
    - Initialize migrations folder structure with 'flask db init'.
    - Adds SQLAlchemy tracking for database schema changes.

--- src/app.py
+++ src/app.py
@@ -1,28 +1,33 @@
+import datetime
 import os
 
-from typing_extensions import Annotated
 import click
 from dotenv import load_dotenv
 from flask import Flask, current_app
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, ForeignKey, func, DateTime
+from sqlalchemy import DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import datetime
+from typing_extensions import Annotated
+
 
 class Base(DeclarativeBase):
     pass
 
 
 db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
 
 
 class User(db.Model):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r}"
+
 
 class Post(db.Model):
     __tablename__ = "post"
@@ -33,7 +38,9 @@ class Post(db.Model):
     author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, title={self.username!r}, author_id{self.author_id!r}"
+        return (
+            f"User(id={self.id!r}, title={self.username!r}, author_id{self.author_id!r}"
+        )
 
 
 @click.command("init-db")
@@ -65,9 +72,10 @@ def create_app(test_config=None):
 
     # initialize extensins
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprint
-    from src.controllers import user, post
+    from src.controllers import post, user
 
     app.register_blueprint(user.pages)
     app.register_blueprint(post.pages)
