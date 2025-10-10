d1d96fa2e6f09a320fe33881c6056a68345002a5
thiagofalima <thiagofalima@hotmail.com>
2025-10-10 20:01:20 -0300

    fix(auth): Change create_access_token argument type from int to str

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -16,5 +16,5 @@ def login():
     if not user or user.password != password:
         return {"message": "Bad username or password"}, HTTPStatus.UNAUTHORIZED
 
-    access_token = create_access_token(identity=user.id)
+    access_token = create_access_token(identity=str(user.id))
     return {"access_token": access_token}
