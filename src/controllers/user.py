a8c6ba77a679aa837d8e61c22903c0bcfb20e2b5
thiagofalima <thiagofalima@hotmail.com>
2025-10-10 20:13:01 -0300

    feat(security): Implement role-based access control (RBAC) decorator
    - Introduces the  decorator to restrict endpoint access by user role.
    - Uses JWT identity to verify the logged-in user's role before allowing access.

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request, session
 from src.app import User, db
 from sqlalchemy import inspect
 from http import HTTPStatus
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
+from src.utils import requires_role
 
 # API RESTFull plural pattern
 pages = Blueprint("user", __name__, url_prefix="/users")
@@ -34,12 +35,7 @@ def _list_users():
 @pages.route("/", methods=["GET", "POST"])
 @jwt_required()
 def hendle_user():
-    user_id = get_jwt_identity()
-    user = db.get_or_404(User, user_id)
-
-    if user.role.name != "admin":
-        return {"message": "User dont have access"}, HTTPStatus.FORBIDDEN
-    
+    requires_role("admin")
     if request.method == "POST":
         try:
             _create_user()
