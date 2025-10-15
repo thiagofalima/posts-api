ef882611a95e584398cb23622a94259f76849189
thiagofalima <thiagofalima@hotmail.com>
2025-10-15 16:46:55 -0300

    fix: Fix relationship string between User and Role

--- src/models/role.py
+++ src/models/role.py
@@ -7,7 +7,7 @@ class Role(db.Model):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    user: Mapped[List["user.User"]] = relationship(back_populates="role")
+    user: Mapped[List["User"]] = relationship(back_populates="role")
 
     def __repr__(self) -> str:
         return f"Role(id={self.id!r}, name={self.name!r}"
--- src/models/user.py
+++ src/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
     role_id: Mapped[int] = mapped_column(ForeignKey("role.id"))
-    role: Mapped["role.Role"] = relationship(back_populates="user")
+    role: Mapped["Role"] = relationship(back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r}"
